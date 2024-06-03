8760e552e9d0999fa4579a44e5e2be91ec01c0e7
martinalupini <martinalupini11@gmail.com>
2024-06-03 19:45:30 +0200

    changed from Junit5 to Junit5 but still getting errors..

--- lang/java/avro/src/test/java/org/apache/avro/TestSchema.java
+++ lang/java/avro/src/test/java/org/apache/avro/TestSchema.java
@@ -3,104 +3,113 @@ package org.apache.avro;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Assert;
 import org.junit.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.invocation.InvocationOnMock;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.mockito.stubbing.Answer;
 
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.apache.avro.Schema.Type.*;
 import static org.apache.avro.Utils.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@RunWith(value= Parameterized.class)
 public class TestSchema {
 
+    private JsonNode schema;
+    private Schema.Names names;
+    private Schema expectedSchema;
+    private Class<Exception> expectedException;
 
-  static Stream<Arguments> provideParseArguments() throws JsonProcessingException {
-    return Stream.of(
+    @Parameters
+    public static Collection<Object[]> getParameters() throws JsonProcessingException {
+      return Arrays.asList(new Object[][]{
 
-        // null Names (i tipi primitivi non dovrebbero avere namespace
-        Arguments.of(getJsonNode(TypeJson.NULL), null, Schema.create(NULL), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.BOOLEAN), null, Schema.create(BOOLEAN), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.INT), null, Schema.create(INT), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.LONG), null, Schema.create(LONG), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.FLOAT), null, Schema.create(FLOAT), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.DOUBLE), null, Schema.create(DOUBLE), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.BYTES), null, Schema.create(BYTES), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.STRING), null, Schema.create(STRING), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.RECORD), null, getRecord(), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.ENUM), null, getEnum(), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.ARRAY), null, getArray(), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.MAP), null, getMap(), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.UNION), null, getUnion(), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.FIXED), null, getFixed(), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.INVALID), null, null, Exception.class),
+          // null Names (i tipi primitivi non dovrebbero avere namespace
+          {getJsonNode(TypeJson.NULL), null, Schema.create(NULL), Exception.class},
+          {getJsonNode(TypeJson.BOOLEAN), null, Schema.create(BOOLEAN), Exception.class},
+          {getJsonNode(TypeJson.INT), null, Schema.create(INT), Exception.class},
+          {getJsonNode(TypeJson.LONG), null, Schema.create(LONG), Exception.class},
+          {getJsonNode(TypeJson.FLOAT), null, Schema.create(FLOAT), Exception.class},
+          {getJsonNode(TypeJson.DOUBLE), null, Schema.create(DOUBLE), Exception.class},
+          {getJsonNode(TypeJson.BYTES), null, Schema.create(BYTES), Exception.class},
+          {getJsonNode(TypeJson.STRING), null, Schema.create(STRING), Exception.class},
+          {getJsonNode(TypeJson.RECORD), null, getRecord(), Exception.class},
+          {getJsonNode(TypeJson.ENUM), null, getEnum(), Exception.class},
+          {getJsonNode(TypeJson.ARRAY), null, getArray(), Exception.class},
+          {getJsonNode(TypeJson.MAP), null, getMap(), Exception.class},
+          {getJsonNode(TypeJson.UNION), null, getUnion(), Exception.class},
+          {getJsonNode(TypeJson.FIXED), null, getFixed(), Exception.class},
+          {getJsonNode(TypeJson.INVALID), null, null, Exception.class},
 
-        // Names validi (ottenuti tramite il costruttore Names("org.apache.avro")
-        Arguments.of(getJsonNode(TypeJson.NULL), new Schema.Names("org.apache.avro"), Schema.create(NULL), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.BOOLEAN), new Schema.Names("org.apache.avro"), Schema.create(BOOLEAN), null),
-        Arguments.of(getJsonNode(TypeJson.INT), new Schema.Names("org.apache.avro"), Schema.create(INT), null),
-        Arguments.of(getJsonNode(TypeJson.LONG), new Schema.Names("org.apache.avro"), Schema.create(LONG), null),
-        Arguments.of(getJsonNode(TypeJson.FLOAT), new Schema.Names("org.apache.avro"), Schema.create(FLOAT), null),
-        Arguments.of(getJsonNode(TypeJson.DOUBLE), new Schema.Names("org.apache.avro"), Schema.create(DOUBLE), null),
-        Arguments.of(getJsonNode(TypeJson.BYTES), new Schema.Names("org.apache.avro"), Schema.create(BYTES), null),
-        Arguments.of(getJsonNode(TypeJson.STRING), new Schema.Names("org.apache.avro"), Schema.create(STRING), null),
-        Arguments.of(getJsonNode(TypeJson.RECORD), new Schema.Names("org.apache.avro"), getRecord(), null),
-        Arguments.of(getJsonNode(TypeJson.ENUM), new Schema.Names("org.apache.avro"), getEnum(), null),
-        Arguments.of(getJsonNode(TypeJson.ARRAY), new Schema.Names("org.apache.avro"), getArray(), null),
-        Arguments.of(getJsonNode(TypeJson.MAP), new Schema.Names("org.apache.avro"), getMap(), null),
-        Arguments.of(getJsonNode(TypeJson.UNION), new Schema.Names("org.apache.avro"), getUnion(), null),
-        Arguments.of(getJsonNode(TypeJson.FIXED), new Schema.Names("org.apache.avro"), getFixed(), null),
-        Arguments.of(getJsonNode(TypeJson.INVALID), new Schema.Names("org.apache.avro"), null, Exception.class),
+          // Names validi (ottenuti tramite il costruttore Names("org.apache.avro")
+          {getJsonNode(TypeJson.NULL), new Schema.Names("org.apache.avro"), Schema.create(NULL), Exception.class},
+          {getJsonNode(TypeJson.BOOLEAN), new Schema.Names("org.apache.avro"), Schema.create(BOOLEAN), null},
+          {getJsonNode(TypeJson.INT), new Schema.Names("org.apache.avro"), Schema.create(INT), null},
+          {getJsonNode(TypeJson.LONG), new Schema.Names("org.apache.avro"), Schema.create(LONG), null},
+          {getJsonNode(TypeJson.FLOAT), new Schema.Names("org.apache.avro"), Schema.create(FLOAT), null},
+          {getJsonNode(TypeJson.DOUBLE), new Schema.Names("org.apache.avro"), Schema.create(DOUBLE), null},
+          {getJsonNode(TypeJson.BYTES), new Schema.Names("org.apache.avro"), Schema.create(BYTES), null},
+          {getJsonNode(TypeJson.STRING), new Schema.Names("org.apache.avro"), Schema.create(STRING), null},
+          {getJsonNode(TypeJson.RECORD), new Schema.Names("org.apache.avro"), getRecord(), null},
+          {getJsonNode(TypeJson.ENUM), new Schema.Names("org.apache.avro"), getEnum(), null},
+          {getJsonNode(TypeJson.ARRAY), new Schema.Names("org.apache.avro"), getArray(), null},
+          {getJsonNode(TypeJson.MAP), new Schema.Names("org.apache.avro"), getMap(), null},
+          {getJsonNode(TypeJson.UNION), new Schema.Names("org.apache.avro"), getUnion(), null},
+          {getJsonNode(TypeJson.FIXED), new Schema.Names("org.apache.avro"), getFixed(), null},
+          {getJsonNode(TypeJson.INVALID), new Schema.Names("org.apache.avro"), null, Exception.class},
 
-        // Names non valido
-        Arguments.of(getJsonNode(TypeJson.NULL), getInvalidNames(), Schema.create(NULL), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.BOOLEAN), getInvalidNames(), Schema.create(BOOLEAN), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.INT), getInvalidNames(), Schema.create(INT), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.LONG), getInvalidNames(), Schema.create(LONG), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.FLOAT), getInvalidNames(), Schema.create(FLOAT), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.DOUBLE), getInvalidNames(), Schema.create(DOUBLE), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.BYTES), getInvalidNames(), Schema.create(BYTES), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.STRING), getInvalidNames(), Schema.create(STRING), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.RECORD), getInvalidNames(), getRecord(), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.ENUM), getInvalidNames(), getEnum(), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.ARRAY), getInvalidNames(), getArray(), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.MAP), getInvalidNames(), getMap(), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.UNION), getInvalidNames(), getUnion(), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.FIXED), getInvalidNames(), getFixed(), Exception.class),
-        Arguments.of(getJsonNode(TypeJson.INVALID), getInvalidNames(), null, Exception.class)
+          // Names non valido
+          {getJsonNode(TypeJson.NULL), getInvalidNames(), Schema.create(NULL), Exception.class},
+          {getJsonNode(TypeJson.BOOLEAN), getInvalidNames(), Schema.create(BOOLEAN), Exception.class},
+          {getJsonNode(TypeJson.INT), getInvalidNames(), Schema.create(INT), Exception.class},
+          {getJsonNode(TypeJson.LONG), getInvalidNames(), Schema.create(LONG), Exception.class},
+          {getJsonNode(TypeJson.FLOAT), getInvalidNames(), Schema.create(FLOAT), Exception.class},
+          {getJsonNode(TypeJson.DOUBLE), getInvalidNames(), Schema.create(DOUBLE), Exception.class},
+          {getJsonNode(TypeJson.BYTES), getInvalidNames(), Schema.create(BYTES), Exception.class},
+          {getJsonNode(TypeJson.STRING), getInvalidNames(), Schema.create(STRING), Exception.class},
+          {getJsonNode(TypeJson.RECORD), getInvalidNames(), getRecord(), Exception.class},
+          {getJsonNode(TypeJson.ENUM), getInvalidNames(), getEnum(), Exception.class},
+          {getJsonNode(TypeJson.ARRAY), getInvalidNames(), getArray(), Exception.class},
+          {getJsonNode(TypeJson.MAP), getInvalidNames(), getMap(), Exception.class},
+          {getJsonNode(TypeJson.UNION), getInvalidNames(), getUnion(), Exception.class},
+          {getJsonNode(TypeJson.FIXED), getInvalidNames(), getFixed(), Exception.class},
+          {getJsonNode(TypeJson.INVALID), getInvalidNames(), null, Exception.class},
 
-    );
-  }
 
-  @ParameterizedTest
-  @MethodSource("provideParseArguments")
-  public void testSchemaParse(JsonNode schema, Schema.Names names, Schema expectedSchema, Class<Exception> expectedException) {
-
-    if (expectedException != null) {
-
-      assertThrows(expectedException, () -> {
-        Schema.parse(schema, names);
       });
-    } else {
-
-      Schema actualSchema =Schema.parse(schema, names);
-
-      assertEquals(expectedSchema, actualSchema);
     }
 
-  }
+    public TestSchema(JsonNode schema, Schema.Names names, Schema expectedSchema, Class<Exception> expectedException){
+        this.schema = schema;
+        this.names = names;
+        this.expectedSchema = expectedSchema;
+        this.expectedException = expectedException;
+    }
+
+    @Test
+    public void testSchemaParse() {
+
+      if (expectedException != null) {
+
+        assertThrows(expectedException, () -> {
+          Schema.parse(schema, names);
+        });
+      } else {
+
+        Schema actualSchema = Schema.parse(schema, names);
+
+        assertEquals(expectedSchema, actualSchema);
+      }
+
+    }
 
 
   private static Schema.Names getInvalidNames(){
